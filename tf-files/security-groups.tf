9119d6a30fa61cf20a91cdfd5b0b623d4b694d8d
sam <meredith101@msn.com>
2025-06-19 14:47:08 +0100

    updated security group

--- tf-files/security-groups.tf
+++ tf-files/security-groups.tf
@@ -1,41 +1,3 @@
-module "eks-security-groups" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
-  cluster_name    = local.cluster_name 
-  cluster_version = "1.29"
-  subnets         = data.aws_subnets.vpc_subnets.ids
-
-  tags = {
-    Environment = "training"
-  }
-
-  vpc_id = aws_default_vpc.default_vpc.id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.nice-cluster-group-1.id]
-      public_ip = true
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.nice-cluster-group-2.id]
-      asg_desired_capacity          = 1
-      public_ip = true
-    }
-  ]
-}
-
-# Security Group 1
 resource "aws_security_group" "nice-cluster-group-1" {
   name_prefix = "nice-cluster-group-1"
   vpc_id      = aws_default_vpc.default_vpc.id
@@ -51,7 +13,6 @@ resource "aws_security_group" "nice-cluster-group-1" {
   }
 }
 
-# Security Group 2
 resource "aws_security_group" "nice-cluster-group-2" {
   name_prefix = "nice-cluster-group-2"
   vpc_id      = aws_default_vpc.default_vpc.id
@@ -62,6 +23,23 @@ resource "aws_security_group" "nice-cluster-group-2" {
     protocol  = "tcp"
 
     cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
       "192.168.0.0/16",
     ]
   }
